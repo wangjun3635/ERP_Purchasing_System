808c39e3454656e0b6ea6ece69843c6dc276c5f5
ynhuang <huangyn@asiainfo.com>
2019-02-20 22:34:16 +0800

    websocket

---
+++ websocket/src/main/java/com/ynhuang/websocket/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.ynhuang.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @Auther:ynhuang
+ * @Date:20/2/19 下午10:31
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
