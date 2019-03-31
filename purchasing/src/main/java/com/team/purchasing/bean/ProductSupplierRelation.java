15b7f64465f5c44c32e28d61ebc7f14a9ed98efc
ynhuang <huangyn@asiainfo.com>
2019-03-31 23:05:28 +0800

    产品查询加入供应商id查询条件

--- purchasing/src/main/java/com/team/purchasing/bean/ProductSupplierRelation.java
+++ purchasing/src/main/java/com/team/purchasing/bean/ProductSupplierRelation.java
@@ -61,6 +61,9 @@ public class ProductSupplierRelation {
     private String brandName;
 
     /** 供应商模块 **/
+    @ApiModelProperty(value = "供应商id")
+    private Integer supplierId;
+
     @ApiModelProperty(value = "供应商名称")
     private String supplierName;
 
